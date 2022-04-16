b99234bb538033fc0e8e38b32359032712194ce7
aetheode <nezhivar@protonmail.com>
2022-04-16 18:29:01 -0400

    update the nxt config to optimize the images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "imgix",
+    path: "https://noop/",
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
+  webpack(config, options) {
+    if (!options.dev) {
+      config.devtool = options.isServer ? false : 'source-map';
+    }
+
+    config.module.rules.push({
+      test: /posts.*\.md/,
+      type: 'asset/source',
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
